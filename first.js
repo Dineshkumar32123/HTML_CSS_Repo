20f6b88270bda62c4537a9fac715ca9fb5964375
Dineshkumar32123 <dineshk32123@gmail.com>
2023-11-01 21:18:28 +0530

    Html and CSS codes

---
+++ first.js
@@ -0,0 +1,15 @@
+var a=10;
+var b=20;
+if(a+b<30){
+  console.log(a+b);
+}
+else if(a+b>30){
+  console.log(a+b);
+}
+else
+  console.log(a+b);
+
+
+for(var num=0;num<=5;num+2){
+  // console.log(num);
+}
